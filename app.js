63f54ac2850173dce26a25731aa1611933160bd8
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2021-12-02 23:46:42 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 function clickHandler(){
-    outputDiv.innerText = "translated: "+textInput.value;
+    outputDiv.innerText = "translated: "+txtInput.value;
 };
 
 btntranslate.addEventListener("click", clickHandler);
