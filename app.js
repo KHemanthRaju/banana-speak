838020970b0eeec2bc7b474b64ca7f63f5fc10b7
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2021-12-10 00:57:27 +0530

    JS work done.

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ var btntranslate = document.querySelector("#btn-translator");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 function getTranslatedURL(text) {
     return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error){
+    console.log("error occured "+error);
+    alert("Something gone wrong with server");
+}
+
 function clickHandler() {
-    // outputDiv.innerText = "translated: "+txtInput.value;
 
     var inputText = txtInput.value;
 
     fetch(getTranslatedURL(inputText))
         .then(response => response.json())
-        .then(json => console.log(json.contents.translated));
+        .then(json => {
+            outputDiv.innerText = json.contents.translated
+        })
+        .catch(errorHandler);
 };
 
 btntranslate.addEventListener("click", clickHandler);
